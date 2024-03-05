377d67115e7e027c8f25564fb869104a9a6691e2
Whitehat-blackclover <vyomshahvira504@gmail.com>
2024-03-05 23:52:23 +0530

    active nav item styling done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import bg from './img/bg.png'
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/Orb/Orb";
+import Navigation from "./components/Navigation/Navigation";
+import React, {useState, useMemo} from 'react'
 function App() {
+  const [active, setActive] = useState(1)
   return (
     <AppStyled bg = {bg} className="App">
-      <Orb></Orb>
       <MainLayout>
-        
+        <Navigation active = {active} setActive={setActive}></Navigation>
       </MainLayout>
     </AppStyled>
   );
